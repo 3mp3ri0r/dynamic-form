302db4c26b7eee24d2f9b223d70d54c9af49cbe0
Christo <cs_sanmar@yahoo.com>
2015-12-22 14:16:04 +0700

    Latest Update

--- views.py
+++ views.py
@@ -3,6 +3,8 @@
 
 from datetime import timedelta
 
+from wheezy.core.collections import attrdict
+from wheezy.core.descriptors import attribute
 from wheezy.web.handlers import BaseHandler
 
 from models import Registration
@@ -10,16 +12,33 @@ from models import Registration
 
 class SignupHandler(BaseHandler):
 
+    @attribute
+    def model(self):
+        return attrdict({
+            'HP': [''],
+        })
+
     def get(self, registration = None):
+        query = self.request.query
         registration = registration or Registration()
+        print(self.model)
+        if 'HP' in query:
+            self.model['HP'].append('')
         return self.render_response(
             'signup.html',
             account=registration.account,
-            phone=registration.phone)
+            phone=registration.phone,
+            model=self.model)
 
     def post(self):
-        registrasi = Registrasi()
-        if (not self.try_update_model(registration)):
+        registration = Registration()
+        if (not self.try_update_model(registration) &
+                self.try_update_model(self.model)):
+            print("Masmasssss!!!!!!!!!!!!!")
             registration.account.password = ''
             return self.get(registration)
+        print("Masuk!!!!!!!!!!!!!")
+        print(registration.account.username)
+        print(self.model.HP[0])
+        print(self.model.HP[1])
         return self.see_other_for('signup')
